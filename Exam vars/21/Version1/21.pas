35cd369e5a30ff996ced0fc9d36d1f30124ed362
Alexander Styagov <sstyagov2@gmail.com>
2017-12-25 00:44:22 +0300

    Checked and fixed realization of 21th variant

--- Exam vars/21/Version1/21.pas
+++ Exam vars/21/Version1/21.pas
@@ -1,63 +1,49 @@
 program ex_21;
 uses crt;
 
-type data = record
-name : string;
-next, last: ^data;
+type
+     data_pointer=^data;
+     data = record
+     name : string;
+     next, last: data_pointer;
 end;
-
-type data_pointer = ^data;
 
 var f_input, f_output:text;
 
-
 procedure load_data(input_file_addr:string; d_root:data_pointer);
-var c_data:^data;
+var c_data: data_pointer;
 f:text;
 begin
 assign(f, input_file_addr);
 reset(f);
-
 c_data := d_root;
-
 while not eoln(f) do begin
 new(c_data^.next);
 c_data^.next^.last := c_data;
 readln(f, c_data^.name);
 c_data := c_data^.next;
 end;
-
 c_data^.last^.next := nil;
 close(f);
-
 end;
-
-
 
 procedure show_list(root:data_pointer; is_straight:boolean);
 begin
-
 while(root <> nil) do begin
 writeln(root^.name);
 if (is_straight) then
-        root := root^.next
+  root := root^.next
 else
-        root:=root^.last;
+  root:=root^.last;
 end;
-
 end;
-
-
-
 
 procedure restore_sorted_data(var root:data_pointer; insert_data:data_pointer);
 var place, last, next:data_pointer;
 found:boolean;
 begin
-
 found := false;
 place := root;
-
 while (place <> nil) and (not found) do
         begin
         if (place^.last = nil) and (insert_data^.name <= place^.name) then
@@ -69,11 +55,9 @@ while (place <> nil) and (not found) do
                       place^.last^.last :=nil;
                       root := place^.last;
                       exit;
-
             end
-
         else
-                 if (place^.next = nil) and (insert_data^.name >= place^.name) then
+        if (place^.next = nil) and (insert_data^.name >= place^.name) then
            begin
                       found := true;
                      new (place^.next);
@@ -83,8 +67,8 @@ while (place <> nil) and (not found) do
                      exit;
 
            end
-              else
-          if (insert_data^.name > place^.name) and ( insert_data^.name < place^.next^.name) then
+        else
+        if (insert_data^.name > place^.name) and ( insert_data^.name < place^.next^.name) then
           begin
                 found:=true;
                 last :=place;
@@ -160,5 +144,5 @@ writeln('_________________________');
 while (f1_data_root^.last <> nil) do
 f1_data_root := f1_data_root^.last;
 show_list(f1_data_root, true);
-readln();
+readkey;
 end.
