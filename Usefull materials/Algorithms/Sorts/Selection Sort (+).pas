c34771c03e04b99ba450e9ef40cf6e8e45485cf9
Alexander Styagov <sstyagov2@gmail.com>
2017-12-25 23:15:51 +0300

    Added Selection Sort by adresses from Fedya

---
+++ Usefull materials/Algorithms/Sorts/Selection Sort (+).pas
@@ -0,0 +1,47 @@
+procedure SortList(var BooksHead: PTBook);
+var
+    BooksI, BooksJ: PTBook;
+    PrevI, PrevJ: PTBook;
+    TempBook: PTBook;
+
+begin
+    BooksI := BooksHead;
+    PrevI := BooksI;
+
+    while BooksI^.SortedNext <> nil do
+    begin
+        BooksI := BooksI^.SortedNext;
+        BooksJ := BooksI;
+        PrevJ := BooksI;
+
+        while BooksJ^.SortedNext <> nil do
+        begin
+            BooksJ := BooksJ^.SortedNext;
+
+            if BooksJ^.Author < BooksI^.Author then
+            begin
+                if BooksI^.SortedNext = BooksJ then
+                begin
+                    PrevI^.SortedNext  := BooksJ;
+                    BooksI^.SortedNext := BooksJ^.SortedNext;
+                    BooksJ^.SortedNext := BooksI;
+                end
+                else
+                begin
+                    PrevI^.SortedNext := BooksJ;
+                    PrevJ^.SortedNext := BooksI;
+                    TempBook := BooksJ^.SortedNext;
+                    BooksJ^.SortedNext := BooksI^.SortedNext;
+                    BooksI^.SortedNext := TempBook;
+                end;
+                TempBook := BooksI;
+                BooksI := BooksJ;
+                BooksJ := TempBook;
+            end;
+
+            PrevJ := BooksJ;
+        end;
+
+        PrevI := BooksI;
+    end;
+end;
